3bf5143fc4253370039937d8b6fabf793c49e0bf
Indika Piyasena <indika.piyasena@gmail.com>
2014-01-07 00:18:54 +1000

    trivial change

--- sync_local.sh
+++ sync_local.sh
@@ -2,4 +2,3 @@
 # Manually copies my blog article from Dropbox to here
 
 cp -f /Users/indika/Plasma/Dev/library/code-library/Blog/_posts/2013-10-25-cooking-with-chef.md _posts/2013-10-25-cooking-with-chef.md
-
