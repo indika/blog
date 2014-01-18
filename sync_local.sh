e8eeda77f806a8063de03ca0358f40174a49edfb
Indika Piyasena <indika.piyasena@gmail.com>
2014-01-18 13:36:44 +1000

    favicon added

--- sync_local.sh
+++ sync_local.sh
@@ -3,4 +3,4 @@
 
 cp -f /Users/indika/Plasma/Dev/library/code-library/Blog/_posts/2013-10-25-cooking-with-chef.md _posts/2013-10-25-cooking-with-chef.md
 git push stable scribble
-git push origin scribble
+git push hub scribble
